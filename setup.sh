85ac75c47e0774e84b90a83ad8125b2fcbcfd325
Luke <lirvin@asu.edu>
2016-06-26 19:43:55 +0000

    Updated setup

--- setup.sh
+++ setup.sh
@@ -5,6 +5,11 @@ sudo apt-get -y install python-pyaudio
 amixer cset numid=3 1
 amixer sset 'PCM' 100%
 
+mkdir /usr/local/bin/sonar
+cp src/* /usr/local/bin/sonar
+sed `cat /etc/rc.local | grep -n "exit 0" | sed "s/:/ /" | awk '{print $1}'`s,exit 0,python /usr/local/bin/sonar/main.py &, /etc/rc.local > /etc/rc.local 
+echo "exit 0" >> /etc/rc.local
+
 apt-get -y remove --purge wolfram-engine triggerhappy anacron logrotate dbus dphys-swapfile xserver-common lightdm
 insserv -r x11-common; apt-get -y autoremove --purge
 
@@ -16,4 +21,9 @@ rm -rf /var/lib/dhcp /var/run /var/spool /var/lock
 ln -s /tmp /var/lib/dhcp; ln -s /tmp /var/run; ln -s /tmp /var/spool; ln -s /tmp /var/lock
 
 sed `cat /etc/systemd/system/dhcpcd5 | grep -n "PIDFile=/run/dhcpcd.pid" | sed "s/:/ /" | awk '{print $1}'`s,run,var/run, /etc/systemd/system/dhcpcd5 > /etc/systemd/system/dhcpcd5 
+
+insserv -r bootlogs; insserv -r console-setup
+
+sed `cat /etc/fstab | grep -n "/dev/mmcblk0p1" | sed "s/:/ /" | awk '{print $1}'`s/defaults/defaults,ro/ /etc/fstab > /etc/fstab 
+sed `cat /etc/fstab | grep -n "/dev/mmcblk0p2" | sed "s/:/ /" | awk '{print $1}'`s/defaults,noatime/defaults,noatime,ro/ /etc/fstab > /etc/fstab 
 
