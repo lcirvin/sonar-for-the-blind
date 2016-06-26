5123367b259a76be23c00b1a9ff9df403bc50199
Luke <lcirvin@asu.edu>
2016-06-26 17:46:21 +0000

    Updated setup

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-sudo apt-get install python-pyaudio
+sudo apt-get -y install python-pyaudio
 
 amixer cset numid=3 1
 amixer sset 'PCM' 100%
 
-apt-get remove --purge wolfram-engine triggerhappy anacron logrotate dbus dphys-swapfile xserver-common lightdm
-insserv -r x11-common; apt-get autoremove --purge
+apt-get -y remove --purge wolfram-engine triggerhappy anacron logrotate dbus dphys-swapfile xserver-common lightdm
+insserv -r x11-common; apt-get -y autoremove --purge
 
-apt-get install busybox-syslogd; dpkg --purge rsyslog
+apt-get -y install busybox-syslogd; dpkg --purge rsyslog
 
-cat /boot/cmdline.txt | sed '/$/ fastboot noswap ro/' > /boot/cmdline.txt
+cat /boot/cmdline.txt | sed 's/$/ fastboot noswap ro/' > /boot/cmdline.txt
 
 rm -rf /var/lib/dhcp /var/run /var/spool /var/lock
 ln -s /tmp /var/lib/dhcp; ln -s /tmp /var/run; ln -s /tmp /var/spool; ln -s /tmp /var/lock
